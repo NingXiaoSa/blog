9f94c9d5d1b73ba355586dfa377041db79e55dd2
Roamantic <1518622781@qq.com>
2021-03-25 17:12:51 +0800

    2021.3.25服务器适配性修改

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -1,13 +1,34 @@
+//package com.example.blog;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class BlogApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(BlogApplication.class, args);
+//    }
+//
+//}
+
+
 package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return super.configure(builder);
+//    }
 }
+
